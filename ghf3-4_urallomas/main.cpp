9d36efa4629e62b985985e4bdf321c64b3d3f925
Szupera Zita <szuperaz@gmail.com>
2014-11-21 17:16:31 +0100

    Initial Commit

---
+++ ghf3-4_urallomas/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ghf3-4_urallomas
+//
+//  Created by Szupera Zita on 21/11/14.
+//  Copyright (c) 2014 Szupera Zita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
